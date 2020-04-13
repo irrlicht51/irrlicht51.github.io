81f0de687a9189885af0289504893a44d28e2539
irrlicht51 <63571049+irrlicht51@users.noreply.github.com>
2020-04-13 12:16:29 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+$(function() {
+  $('.burger-btn').click(function() {
+    $('.header-navi').fadeToggle(300);
+    $(this).toggleClass('close');
+    $('body').toggleClass('noscroll');
+  });
+
+  if ($(window).width() < 768 ) {
+    $('.header-navi ul li>a').click(function() {
+      $('.header-navi').fadeToggle(300);
+      $('.burger-btn').removeClass('close');
+      $('body').removeClass('noscroll');
+    });
+  }
+
+  $('a[href^="#"]').click(function() {
+    var speed = 500;
+    var href = $(this).attr('href');
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('html,body').animate( {
+      scrollTop: position
+    }, speed, 'swing');
+    return false;
+  });
+
+  var pageTop = $('#page-top');
+
+  pageTop.hide();
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100 ) {
+      pageTop.fadeIn();
+    } else {
+      pageTop.fadeOut();
+    }
+  });
+
+  pageTop.click(function() {
+    $('html,body').animate( {
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
+});
